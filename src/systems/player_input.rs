3c7d43fb6e017959cf3693b6115a6738aec0c749
marco tosini <>
2022-11-13 10:26:20 +0100

    added recoery

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     world: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -27,6 +29,7 @@ pub fn player_input(
 
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
 
+        let mut did_something = false;
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
             enemies
@@ -34,6 +37,7 @@ pub fn player_input(
                 .filter(|(entity, enemy_position)| destination == **enemy_position)
                 .for_each(|(enemy, _)| {
                     hit_something = true;
+                    did_something = true;
                     commands.push((
                         (),
                         WantToAttack {
@@ -51,6 +55,16 @@ pub fn player_input(
                         destination,
                     },
                 ));
+            }
+
+            if !did_something {
+                if let Ok(mut health) = world
+                    .entry_mut(player)
+                    .unwrap()
+                    .get_component_mut::<Health>()
+                {
+                    health.current = i32::min(health.max, health.current + 1);
+                }
             }
             *turn_state = TurnState::PlayerTurn;
         }
